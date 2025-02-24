4ece034c933a743a7329282b8301b4ed2b21f673
bansalj <jagvirbansal@gmail.com>
2025-02-24 21:09:21 +0000

    Began coordinate system for image

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 interface IProp {
     onClick: () => void;
 }
+
+interface event extends Event {
+    clientX: string,
+    clientY: string
+}
+
+let mouse: event
 
 export default function Index() {
 
@@ -12,7 +19,7 @@ export default function Index() {
             <div className="content">
                 <div className="container">
                     <img src="925901.jpg" onClick={(event: React.MouseEvent<HTMLElement>) => {
-                        console.log("hello")
+                        console.log({X: event.clientX, Y: event.clientY})
                         }} alt="" style={{width:"90%"}}/>
                 </div>
                 <div><code>: <span style={{color:"white"}}>n/a</span></code></div>
