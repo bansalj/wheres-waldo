b4b64364f06b054a726a43c3fd74de429a3a1215
bansalj <jagvirbansal@gmail.com>
2025-02-26 21:17:31 +0000

    Began logic for mouse pointer menu appearance on click

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-interface IProp {
-    onClick: () => void;
-}
 
-interface event extends Event {
-    clientX: string,
-    clientY: string
-}
-
-let mouse: event
 
 export default function Index() {
+
+    const [visibility, setVisibility] = useState("hidden");
+
+    function pointerDown(event: React.MouseEvent<HTMLElement>) {
+        console.log(`X: ${event.screenX}, Y: ${event.screenY}`)
+            setVisibility("visible");
+    }
+
+    function pointerUp(event: React.MouseEvent<HTMLElement>) {
+        setVisibility("hidden");
+    }
 
     return (
         <>
             <div className="content">
                 <div className="container">
-                    <img src="925901.jpg" onClick={(event: React.MouseEvent<HTMLElement>) => {
-                        console.log({X: event.clientX, Y: event.clientY})
-                        }} alt="" style={{width:"90%"}}/>
+                    <img src="925901.jpg" onPointerDown={pointerDown} onPointerUp={pointerUp} style={{height:"90%", width:"100%"}}/>   
                 </div>
-                <div><code>: <span style={{color:"white"}}>n/a</span></code></div>
+                <div className={visibility}>
+                    <Menu/>
+                </div>
             </div>
         </>
     )
 
 }
+
+function Menu({}) {
+    return (
+        <>
+            <label>
+                Choose:
+                <select name="options" id="">
+                    <option value="Waldo">Waldo</option>
+                    <option value="NPC1">NPC1</option>
+                    <option value="NPC2">NPC2</option>
+                </select>
+            </label>
+        </>
+    )
+}  
