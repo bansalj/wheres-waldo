6db936c95f7b2ab51daf1ed09b0dfc02c578e769
bansalj <jagvirbansal@gmail.com>
2025-02-27 22:05:35 +0000

    updated onClick functionality, menu locks to mouse position

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,15 @@ import { Link } from "react-router-dom";
 export default function Index() {
 
     const [visibility, setVisibility] = useState("hidden");
-
+    const [styleTop, setStyleTop] = useState(0);
+    const [styleLeft, setStyleLeft] = useState(0);
+ 
     function pointerDown(event: React.MouseEvent<HTMLElement>) {
-        console.log(`X: ${event.screenX}, Y: ${event.screenY}`)
+        console.log(`X: ${event.clientX}, Y: ${event.clientY}`)
             setVisibility("visible");
+            setStyleTop(event.clientY - 20);
+            setStyleLeft(event.clientX - 50);
+
     }
 
     function pointerUp(event: React.MouseEvent<HTMLElement>) {
@@ -20,10 +25,12 @@ export default function Index() {
         <>
             <div className="content">
                 <div className="container">
-                    <img src="925901.jpg" onPointerDown={pointerDown} onPointerUp={pointerUp} style={{height:"90%", width:"100%"}}/>   
+                    <img src="925901.jpg" onPointerDown={pointerDown}  style={{height:"90%", width:"100%"}}/>   
                 </div>
-                <div className={visibility}>
-                    <Menu/>
+                <div className={visibility}  >
+                    <Menu 
+                    styleLeft={styleLeft}
+                    styleTop={styleTop}/>
                 </div>
             </div>
         </>
@@ -31,12 +38,12 @@ export default function Index() {
 
 }
 
-function Menu({}) {
+function Menu({ styleTop, styleLeft }) {
     return (
         <>
             <label>
                 Choose:
-                <select name="options" id="">
+                <select name="options" id="" className="menu" style={{top: styleTop + "px", left: styleLeft + "px"}}>
                     <option value="Waldo">Waldo</option>
                     <option value="NPC1">NPC1</option>
                     <option value="NPC2">NPC2</option>
