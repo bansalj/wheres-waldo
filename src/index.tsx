afc6b6359627943ac7c59d3e8036bc33baab9f99
bansalj <jagvirbansal@gmail.com>
2025-03-04 22:02:39 +0000

    adjusted image size to get accurate map coordinates

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-
 
 export default function Index() {
 
     const [visibility, setVisibility] = useState("hidden");
     const [styleTop, setStyleTop] = useState(0);
     const [styleLeft, setStyleLeft] = useState(0);
+    const [clicked, setClicked] = useState(false);
  
     function pointerDown(event: React.MouseEvent<HTMLElement>) {
-        console.log(`X: ${event.clientX}, Y: ${event.clientY}`)
+        setClicked(true);
+        if (clicked == true) {
+            console.log(`X: ${event.pageX}, Y: ${event.pageY}`)
             setVisibility("visible");
             setStyleTop(event.clientY - 20);
-            setStyleLeft(event.clientX - 50);
-
+            setStyleLeft(event.clientX + 10);
+            setClicked(false);
+        } else if (clicked == false) {
+            setVisibility("hidden");
+        }       
     }
 
-    function pointerUp(event: React.MouseEvent<HTMLElement>) {
-        setVisibility("hidden");
+    function isWaldo(event: React.MouseEvent<HTMLElement>) {
+        
+    }
+
+    function waldo(event: React.MouseEvent<HTMLElement>) {
+        event.preventDefault();
+        console.log("You found Waldo!")      
     }
 
     return (
         <>
             <div className="content">
                 <div className="container">
-                    <img src="925901.jpg" onPointerDown={pointerDown}  style={{height:"90%", width:"100%"}}/>   
-                </div>
-                <div className={visibility}  >
-                    <Menu 
+                    <img src="925901.jpg" useMap="#areamap" onClick={pointerDown}/>   
+                <map name="areamap">
+                    <area coords="2531,1348,2605,1473" alt="map" href="null" onClick={waldo}/>
+                </map>
+                <div className={visibility}>
+                    <Menu
                     styleLeft={styleLeft}
                     styleTop={styleTop}/>
                 </div>
+                </div>  
             </div>
         </>
     )
@@ -41,14 +52,21 @@ export default function Index() {
 function Menu({ styleTop, styleLeft }) {
     return (
         <>
-            <label>
-                Choose:
-                <select name="options" id="" className="menu" style={{top: styleTop + "px", left: styleLeft + "px"}}>
-                    <option value="Waldo">Waldo</option>
-                    <option value="NPC1">NPC1</option>
-                    <option value="NPC2">NPC2</option>
-                </select>
-            </label>
+            <div className="menu" style={{top: styleTop + "px", left: styleLeft + "px"}}>    
+                <button>Waldo</button>
+                <button>NPC1</button>
+                <button>NPC2</button>
+            </div>
         </>
     )
-}  
+}
+
+// function Map({ waldo }) {
+//     return (
+//         <>
+//             <map name="areamap">
+//                 <area shape="rect" coords="1629, 1053, 1662, 1123" href="925901.jpg" onClick={waldo}/>
+//             </map>
+//         </>
+//     )
+// }
