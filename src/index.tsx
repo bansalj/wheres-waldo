786ce364457b864746938c3e3b4e5a405704049a
bansalj <jagvirbansal@gmail.com>
2025-03-19 22:18:03 +0000

    Created logic to find and verify areas with characters has been clicked

--- src/index.tsx
+++ src/index.tsx
@@ -9,28 +9,7 @@ export default function Index() {
     const [character1, setCharater1] = useState<any[]>([]);
     const [character2, setCharater2] = useState<any[]>([]);
     const [character3, setCharater3] = useState<any[]>([]);
- 
-    function pointerDown(event: React.MouseEvent<HTMLElement>) {
-        event.preventDefault();
-        setClicked(true);
-        if (clicked) {
-            menuPosition(event);
-            if(event.target.coords) {
-                check(event.target.coords);
-            }
-            
-            setClicked(false);
-        } else {
-            setVisibility("hidden");
-        }       
-    }
-
-    function menuPosition(event: React.MouseEvent<HTMLElement>) {
-        console.log(`X: ${event.pageX}, Y: ${event.pageY}`)
-        setVisibility("visible");
-        setStyleTop(event.clientY - 20);
-        setStyleLeft(event.clientX + 10);
-    }
+    const [selectedName, setSelectedName] = useState<string>();
 
     useEffect(() => {
         fetch("https://localhost:7057/api/character", {
@@ -41,24 +20,53 @@ export default function Index() {
         .then((character:any) => {
             setCharater1(character[0]);
             setCharater2(character[1]);
-            setCharater3(character[2]); 
+            setCharater3(character[2]);
         })
         .catch(error => console.error("Nerwork Error", error));
     },[setCharater1, setCharater2, setCharater3])
+ 
+    function pointerDown(event: React.MouseEvent<HTMLElement>) {
+        event.preventDefault();
+        setClicked(true);
+        setSelectedName(event.target.alt);
+        console.log(selectedName);
+        if (clicked) {
+            menuPosition(event);
+            setClicked(false);
+        } else {
+            setVisibility("hidden");
+        }       
+    }
 
+    function menuPosition(event: React.MouseEvent<HTMLElement>) {
+        setVisibility("visible");
+        setStyleTop(event.clientY - 20);
+        setStyleLeft(event.clientX + 10);
+    }
 
-    function check(event:any) {
-        switch(event) {
-            case `${character1.coord}`:
+    function check(event: React.MouseEvent<HTMLElement>) {
+       let name: string = event.target.innerText;
+       if (name == selectedName) {
+        switch(name) {
+            case (character1.name):
                 console.log(`You found ${character1.name}!`);
+                setVisibility("hidden");
                 break;
-            case `${character2.coord}`:
+            case (character2.name):
                 console.log(`You found ${character2.name}!`);
+                setVisibility("hidden");
                 break;
-            case `${character3.coord}`:
+            case (character3.name):
                 console.log(`You found ${character3.name}!`);
+                setVisibility("hidden");
                 break;
+            default:
+                console.log("Sorry, try again!");
         }
+       } else {
+        console.log("Sorry! Try again!");
+        setVisibility("hidden");
+       }        
     }
 
     const validate = async(event: React.MouseEvent<HTMLElement>) => {
@@ -71,9 +79,9 @@ export default function Index() {
                 <div className="container">
                     <img src="925902.jpg" useMap="#areamap" onClick={pointerDown}/>   
                 <map name="areamap">
-                    <area coords={String(character1.coord)} alt="Waldo" href="null" onClick={pointerDown}/>
-                    <area coords={String(character2.coord)} alt="map2" href="null" onClick={pointerDown}/>
-                    <area coords={String(character3.coord)} alt="map3" href="null" onClick={pointerDown}/>
+                    <area coords={String(character1.coord)} alt={`${character1.name}`} href="null" onClick={pointerDown}/>
+                    <area coords={String(character2.coord)} alt={`${character2.name}`} href="null" onClick={pointerDown}/>
+                    <area coords={String(character3.coord)} alt={`${character3.name}`} href="null" onClick={pointerDown}/>
                 </map>
                 <div className={visibility}>
                     <Menu
@@ -81,7 +89,8 @@ export default function Index() {
                     styleTop={styleTop}
                     name1={character1.name}
                     name2={character2.name}
-                    name3={character3.name}/>
+                    name3={character3.name}
+                    onClick={check}/>
                 </div>
                 </div>  
             </div>
@@ -90,13 +99,13 @@ export default function Index() {
 
 }
 
-function Menu({ styleTop, styleLeft, name1, name2, name3 }) {
+function Menu({ styleTop, styleLeft, name1, name2, name3, onClick }) {
     return (
         <>
             <div className="menu" style={{top: styleTop + "px", left: styleLeft + "px"}}>    
-                <button>{name1}</button>
-                <button>{name2}</button>
-                <button>{name3}</button>
+                <button onClick={onClick}>{name1}</button>
+                <button onClick={onClick}>{name2}</button>
+                <button onClick={onClick}>{name3}</button>
             </div>
         </>
     )
